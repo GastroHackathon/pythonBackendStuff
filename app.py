b093b1c0c084dba4e0adea57d33202954c412c62
thegreatraphael <Raphael.Beyweiss@live.com>
2019-11-09 23:41:26 +0100

    fix requests

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from util.Icons import get_ihate
 from util.Icons import get_ilove
 from util.Icons import get_icant
 
+from flask_cors import CORS
+
 from util.Images import get_image_list
 
 from util.Questions import get_first_question_response
@@ -14,9 +16,12 @@ from util.Questions import get_third_question_response
 from util.Questions import get_final_flag
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/profileGeneral/<art>', methods=['GET'])
-def getProfileGeneral(art):
+
+@app.route('/profileGeneral', methods=['GET'])
+def getProfileGeneral():
+    art = request.args.get('art')
     if art == 'ILove':
         return jsonify(get_ilove())
     elif art == 'IHate':
@@ -24,22 +29,27 @@ def getProfileGeneral(art):
     elif art == 'ICant':
         return jsonify(get_icant())
 
-@app.route('/profileGeneral', methods=['POST'])
+
+@app.route('/profileGeneralPost', methods=['POST'])
 def postProfileGeneral():
     print(request.json)
     # TODO do something with the data
 
+
 @app.route('/profileFood', methods=['GET'])
 def getProfileFood():
     return jsonify(get_image_list())
+
 
 @app.route('/profileFood', methods=['POST'])
 def postProfileFood():
     print(request.json)
     # TODO do something with the data
 
-@app.route('/question/<cnt>', methods=['GET'])
-def question(cnt):
+
+@app.route('/question', methods=['GET'])
+def question():
+    cnt = request.args.get('cnt')
     if cnt == 1:
         return jsonify(get_first_question_response())
     elif cnt == 2:
@@ -49,9 +59,11 @@ def question(cnt):
     elif cnt == 4:
         return jsonify(get_final_flag())
 
+
 @app.route('/results')
 def results():
     return None
+
 
 if __name__ == '__main__':
     app.run()
