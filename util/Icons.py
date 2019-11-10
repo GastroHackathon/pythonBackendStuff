153ddfb70a1a11682a64148124b8909493eb6837
Rene Clemens Haubitzer <haubitzer@student.tugraz.at>
2019-11-10 02:32:54 +0100

    asd

--- util/Icons.py
+++ util/Icons.py
@@ -1,24 +1,25 @@
-def get_veggie_response():
-    return {'id': 1, 'name': ['fa', 'carrot']}
+def get_veggie_response(art):
+    return {'id': 1, 'name': ['fa', 'carrot'], 'art': art}
 
-def get_vegan_response():
-    return {'id': 2, 'name': ['fa', 'seedling']}
+def get_vegan_response(art):
+    return {'id': 2, 'name': ['fa', 'seedling'], 'art': art}
 
-def get_gluten_response():
-    return {'id': 3, 'name': ['fa', 'bread-slice']}
+def get_gluten_response(art):
+    return {'id': 3, 'name': ['fa', 'bread-slice'], 'art': art}
 
-def get_shrimp_response():
-    return {'id': 4, 'name': ['fa', 'fish']}
+def get_shrimp_response(art):
+    return {'id': 4, 'name': ['fa', 'fish'], 'art': art}
 
-def get_milk_response():
-    return {'id': 5, 'name': ['fa', 'blender']}
+def get_milk_response(art):
+    return {'id': 5, 'name': ['fa', 'blender'], 'art': art}
+
 
 def get_ilove():
-    return [get_veggie_response(), get_vegan_response()]
+    return [get_veggie_response('ILove'), get_vegan_response('ILove')]
 
 def get_ihate():
-    return [get_veggie_response(), get_vegan_response()]
+    return [get_veggie_response('IHate'), get_vegan_response('IHate')]
 
 def get_icant():
-    return [get_gluten_response(), get_shrimp_response(), get_milk_response()]
+    return [get_gluten_response('ICant'), get_shrimp_response('ICant'), get_milk_response('ICant')]
 
